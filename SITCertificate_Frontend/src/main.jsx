c03f85e3b8b4d2288936ba70aea260d36cb6eeda
MS. NANNICHA PHRAEMETTA <66130500846@st.sit.kmutt.ac.th>
2024-06-13 15:30:11 +0700

    Login Prototype

--- SITCertificate_Frontend/src/main.jsx
+++ SITCertificate_Frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import * as ReactDOM from 'react-dom/client'
-
+import UserLoginPage from './pages/UserLoginPage'
+import './index.css'
 const rootElement = document.getElementById('root')
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <UserLoginPage />
     </ChakraProvider>
   </React.StrictMode>,
 )
