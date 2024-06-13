8ef744b2184136851da2ae6483198bee7b764625
Nudhana Sarutipaisan <snudhana@Nudhanas-MacBook-Pro.local>
2024-06-13 15:01:07 +0700

    asda

---
+++ SITCertificate_Frontend/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Box, Button, Text } from '@chakra-ui/react'
+
+export default function Navbar() {
+    return (
+        <Box bgColor="#0c2d4e">
+            <Text color='white'>SITCertificate</Text>
+        </Box>
+    )
+}
