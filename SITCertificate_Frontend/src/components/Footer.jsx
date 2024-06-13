d353fc8a449613b2286336f3078c345efe03985b
Nudhana Sarutipaisan <snudhana@Nudhanas-MacBook-Pro.local>
2024-06-13 18:11:38 +0700

    add navbar

---
+++ SITCertificate_Frontend/src/components/Footer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Box, Text, Divider, Image, Center } from '@chakra-ui/react';
+import { Link } from "react-router-dom"
+import { FaPhone, FaEnvelope, FaFacebook, FaLine } from "react-icons/fa6";
+import logo from "../assets/img/SIT_logo.png";
+
+export default function Footer() {
+    return (
+        <>
+            <Box bgColor='#0c2d4e'>
+                <Box display='flex' flexDirection={{ base: "column", sm: "row" }} alignItems='center' justifyContent='space-evenly' pb='10px'>
+                    <Box>
+                        <Link to="https://www.sit.kmutt.ac.th/">
+                            <Image src={logo} alt="Logo" width={{ base: '300px', lg: "500px" }} height='auto' />
+                        </Link>
+                    </Box>
+                    <Box color='whitesmoke' fontSize={{ xs: "12px", md: "14px", lg: '16px' }}>
+                        <Link to="" className="hover:text-white">
+                            <Box display='flex' alignItems='center' pt={{ base: '28px', md: '40px' }} _hover={{ textDecoration: 'underline', textUnderlineOffset: '2px', color: 'white' }}>
+                                <FaPhone /> <Text pl='5px'> +66 2470 9850</Text>
+                            </Box>
+                        </Link>
+                        <Link to="mailto:webadmin@sit.kmutt.ac.th" className="hover:text-white">
+                            <Box display='flex' alignItems='center' pt='10px' _hover={{ textDecoration: 'underline', textUnderlineOffset: '2px', color: 'white' }}>
+                                <FaEnvelope /> <Text pl='5px'> webadmin@sit.kmutt.ac.th</Text>
+                            </Box>
+                        </Link>
+                        <Link to="https://www.facebook.com/SIT.Family" className="hover:text-white flex pt-[10px]">
+                            <Box display='flex' alignItems='center' pt='10px' _hover={{ textDecoration: 'underline', textUnderlineOffset: '2px', color: 'white' }}>
+                                <FaFacebook /> <Text pl='5px'> SIT.Family</Text>
+                            </Box>
+                        </Link>
+                        <Link to="https://page.line.me/olt5471s?openQrModal=true" className="hover:text-white flex pt-[10px] pb-[20px]">
+                            <Box display='flex' alignItems='center' pt='10px' _hover={{ textDecoration: 'underline', textUnderlineOffset: '2px', color: 'white' }}>
+                                <FaLine /> <Text pl='5px'> @sit.kmutt</Text>
+                            </Box>
+                        </Link>
+                    </Box>
+                </Box>
+                <Center>
+                    <Divider width="70%" mt="3" mb="2" />
+                </Center>
+                <Text display='flex' justifyContent='center' textAlign='center' mt='1%' pb='2%' color='#E0E5E5' fontSize={{ base: '9px',  md: "12px",lg: "14px"}}>
+                    © 2018 School of Information Technology, King Mongkut's University of Technology Thonburi.
+                </Text>
+            </Box>
+        </>
+    )
+}
--- SITCertificate_Frontend/src/components/Navbar.jsx
+++ SITCertificate_Frontend/src/components/Navbar.jsx
@@ -1,21 +1,78 @@
-import React from 'react'
-import { Box, Button, Text, IconButton } from '@chakra-ui/react'
-import { FaArrowRightFromBracket } from "react-icons/fa6";
-import Cookies from "js-cookie";
+import React, { useState } from 'react';
+import { Box, Button, Text, IconButton } from '@chakra-ui/react';
+import { FaArrowRight } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 export default function Navbar() {
-    return (
-        <Box bgColor="#0c2d4e" display='flex' justifyContent='space-between' alignItems='center' p='20px 50px'>
-            <Text color='white' fontWeight='bold' fontSize='24' >SITCertificate</Text>
-            <Button leftIcon={<FaArrowRightFromBracket />} bgColor='#336699' color='white' size='md' display={{ base: 'none', md: 'flex' }} _hover={{ bgColor: '#1f568c' }} variant='solid'
-                onClick={() => {
-                    Cookies.remove("token");
-                    // navigate("/");
-                    window.location.reload();
-                }}>
-                ออกจากระบบ
-            </Button>
-            <IconButton justifyContent='center' bgColor='#336699' color='white' display={{ base: 'flex', md: 'none' }} _hover={{ bgColor: '#1f568c' }} icon={<FaArrowRightFromBracket />} />
-        </Box>
-    )
+  const navigate = useNavigate();
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [shouldShowNavbar, setShouldShowNavbar] = useState(true);
+
+  const handleScroll = () => {
+    const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScrollTop > lastScrollTop) {
+      setShouldShowNavbar(false);
+    } else {
+      setShouldShowNavbar(true);
+    }
+
+    setLastScrollTop(currentScrollTop <= 0 ? 0 : currentScrollTop);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [lastScrollTop]);
+
+  return (
+    <Box
+      bgColor="#0c2d4e"
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      p="20px 50px"
+      position="fixed"
+      top="0"
+      left="0"
+      right="0"
+      zIndex="999"
+      transition="top 0.3s"
+      {...(shouldShowNavbar ? {} : { top: '-80px' })}
+    >
+      <Text color="white" fontWeight="bold" fontSize="24">
+        SITCertificate
+      </Text>
+      <Button
+        leftIcon={<FaArrowRight />}
+        bgColor="#336699"
+        color="white"
+        size="md"
+        display={{ base: 'none', md: 'flex' }}
+        _hover={{ bgColor: '#1f568c' }}
+        variant="solid"
+        onClick={() => {
+          Cookies.remove('token');
+          navigate('/');
+        }}
+      >
+        ออกจากระบบ
+      </Button>
+      <IconButton
+        justifyContent="center"
+        bgColor="#336699"
+        color="white"
+        display={{ base: 'flex', md: 'none' }}
+        _hover={{ bgColor: '#1f568c' }}
+        icon={<FaArrowRight />}
+        onClick={() => {
+          Cookies.remove('token');
+          navigate('/');
+        }}
+      />
+    </Box>
+  );
 }
