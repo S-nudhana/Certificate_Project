2a7a723ce89b528a39635eb8ac9d8ab01ded75dd
Nudhana Sarutipaisan <snudhana@Nudhanas-MacBook-Pro.local>
2024-06-13 15:19:03 +0700

    adas

--- SITCertificate_Frontend/src/main.jsx
+++ SITCertificate_Frontend/src/main.jsx
@@ -1,11 +1,20 @@
 import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import * as ReactDOM from 'react-dom/client'
+
+import App from './App';
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Noto Sans Thai', sans-serif`,
+    body: `'Noto Sans Thai', sans-serif`,
+  },
+});
 
 const rootElement = document.getElementById('root')
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
