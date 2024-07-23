8683e054fe5b481c21c8cabf2b5f39ac555b33b9
Nudhana Sarutipaisan <nu37317@gmail.com>
2024-07-23 21:10:19 +0700

    PDF fixed

--- SITCertificate_Frontend/src/components/PdfViewer.jsx
+++ SITCertificate_Frontend/src/components/PdfViewer.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
 import { Worker, Viewer } from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css';
+import PDF from 'react-pdf-watermark';
 
 export default function PdfViewer({ fileUrl }) {
   return (
-    <Box pb={'20px'} mt={'20px'}>
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174 /build/pdf.worker.min.js">
-        <Box width={'100%'} height={'100%'}>
-          <Viewer fileUrl={fileUrl} />
-        </Box>
-      </Worker>
+    <Box mb={'20px'} mt={'20px'} boxShadow={'lg'} width={'90%'} height={'auto'}>
+      <PDF
+        canvasWidth={'100%'}
+        canvasHeight={'auto'}
+        file={fileUrl}
+      />
     </Box>
   );
 }
