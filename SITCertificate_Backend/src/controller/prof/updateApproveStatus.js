8a61369f6758e62aea9264d2fff0bd0ef0e29756
Nudhana Sarutipaisan <79238555+S-nudhana@users.noreply.github.com>
2024-09-14 11:17:29 +0700

    Update updateApproveStatus.js

--- SITCertificate_Backend/src/controller/prof/updateApproveStatus.js
+++ SITCertificate_Backend/src/controller/prof/updateApproveStatus.js
@@ -1,51 +1,3 @@
-// import db from "../../db/connection.js";
-// import XLSX from "xlsx";
-
-// const updateApproveStatus = async(req, res) => {
-//     const { eventId } = req.body;
-//     try {
-//         const excelQuery = await db
-//             .promise()
-//             .query("SELECT event_excel FROM event WHERE event_Id = ?", [eventId]);
-//         const excel = excelQuery[0][0].event_excel;
-//         console.log(excel)
-//         if (!excel) {
-//             return res.status(400).send("No file uploaded.");
-//         }
-//         const workbook = XLSX.readFile(excel);
-//         const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-//         const sheetData = XLSX.utils.sheet_to_json(worksheet);
-//         const columnAData = sheetData.map((row) => row.email || "");
-//         const columnBData = sheetData.map((row) => row.Mobile || "");
-//         const studentData = columnAData.map((name, index) => [
-//             columnAData[index],
-//             columnBData[index],
-//             eventId,
-//             0,
-//         ]);
-//         await db.promise().query(
-//             "INSERT INTO student (student_email, student_password, student_joinEventId, student_eventGenerated) VALUES ?", [studentData]
-//         );
-//         await db
-//             .promise()
-//             .query("UPDATE event SET event_approve = ? WHERE event_Id = ?", [
-//                 1,
-//                 eventId,
-//             ]);
-//         return res.json({
-//             success: true,
-//             error: null,
-//         });
-//     } catch (error) {
-//         console.log("Error:", error);
-//         return res.status(500).json({
-//             success: false,
-//             data: null,
-//             error: error.message,
-//         });
-//     }
-// };
-// export default updateApproveStatus;
 import db from "../../db/connection.js";
 import XLSX from "xlsx";
 import axios from 'axios';
@@ -54,7 +6,6 @@ import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-// Define __dirname for ES modules
 const __filename = fileURLToPath(
     import.meta.url);
 const __dirname = dirname(__filename);
