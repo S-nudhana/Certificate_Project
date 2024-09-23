0d25ca95b7952ce718e7d212c61fb276183a7397
Nudhana Sarutipaisan <nu37317@gmail.com>
2024-09-23 23:18:44 +0700

    professor forget pass  update

--- SITCertificate_Backend/src/controller/auth/crypto.js
+++ SITCertificate_Backend/src/controller/auth/crypto.js
@@ -2,24 +2,6 @@ import crypto from "crypto";
 import dotenv from "dotenv";
 dotenv.config();
 
-// export const encryptPin = (pin) => {
-//     const ciphertext = CryptoJS.AES.encrypt(
-//         pin.toString(),
-//         process.env.PINSecretKey
-//     ).toString();
-//     // const cryptoKey = crypto.createCipheriv('aes-256-ecb', process.env.PINSecretKey, '');
-//     // const ciphertext = cryptoKey.update(pin.toString(), 'utf8', 'hex') + cryptoKey.final('hex');
-//     return ciphertext;
-// };
-
-// export const decryptPin = (ciphertext) => {
-//     const bytes = CryptoJS.AES.decrypt(ciphertext, process.env.PINSecretKey);
-//     const decryptedPin = bytes.toString(CryptoJS.enc.Utf8);
-//     // const cryptoKey = crypto.createDecipheriv('aes-256-ecb', process.env.PINSecretKey, '');
-//     // const decryptedPin = cryptoKey.update(ciphertext, 'hex', 'utf8') + cryptoKey.final('utf8');
-//     return parseInt(decryptedPin, 10);
-// };
-
 export const encryptPin = (pin) => {
     const key = crypto.scryptSync(process.env.PINSecretKey, 'salt', 32);
     const iv = crypto.randomBytes(16);
