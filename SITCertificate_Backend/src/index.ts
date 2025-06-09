a3767f8aa05ca04485dd5f6a7dbf5ac8472d4786
Nudhana Sarutipaisan <79238555+S-nudhana@users.noreply.github.com>
2025-06-09 23:35:43 +0700

    Update index.ts

--- SITCertificate_Backend/src/index.ts
+++ SITCertificate_Backend/src/index.ts
@@ -21,11 +21,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors(corsOptions));
 app.use(logger);
-app.use(
-  helmet({
-    contentSecurityPolicy: false,
-  })
-);
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
